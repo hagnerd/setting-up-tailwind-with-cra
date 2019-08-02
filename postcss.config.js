021363b175603e5028ad42dfa201336fb29358eb
Matt Hagner <web@matthagner.com>
2019-08-02 07:49:21 -0500

    Fix postcss.config for purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
     './src/**/*.js',
+    './public/index.html'
   ],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 })
--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import Button from "./components/button";
 
 function App() {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('hi from production')
+  }
   return (
-    <div>
+    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
       <h1>Super cool page</h1>
       <Button onClick={() => console.log("I was clicked")}>
         I am a button
